0b02c632b5a8c29776300707af1b861041f1eb39
Barry <barry@endzone.io>
2019-11-02 20:48:49 +0000

    Updates for testing locally

--- app.php
+++ app.php
@@ -10,22 +10,7 @@ use Illuminate\Database\Capsule\Manager as Capsule;
 
 $capsule = new Capsule;
 
-if ($_SERVER['HTTP_HOST'] == $_SERVER['HTTP_HOST']) {
-	//hosted testing version
-	$capsule->addConnection([
-		'driver'    => 'mysql',
-		'host'      => 'localhost',
-		'database'  => 'liveplay_designer_api',
-		'username'  => 'liveplay_wpnew',
-		'password'  => ')QkP?UsK]*Iv',
-		'charset'   => 'utf8',
-		'collation' => 'utf8_unicode_ci',
-		'prefix'    => '',
-	]);
-
-	define('APPROOT_URL', "http://".$_SERVER['SERVER_NAME'] . "/designer_api" . DIRECTORY_SEPARATOR);
-
-} else {
+if (strpos($_SERVER['HTTP_HOST'], '.local') !== false) {
 	//local testing version
 	$capsule->addConnection([
 		'driver'    => 'mysql',
@@ -38,6 +23,21 @@ if ($_SERVER['HTTP_HOST'] == $_SERVER['HTTP_HOST']) {
 		'prefix'    => '',
 	]);
 	define('APPROOT_URL', "http://".$_SERVER['SERVER_NAME'] ."/RHoK-playgroundideas-app-api/");
+} else {
+	//hosted version
+	$capsule->addConnection([
+		'driver'    => 'mysql',
+		'host'      => 'localhost',
+		'database'  => '{database}',
+		'username'  => '{username}',
+		'password'  => '{password}',
+		'charset'   => 'utf8',
+		'collation' => 'utf8_unicode_ci',
+		'prefix'    => '',
+	]);
+
+	define('APPROOT_URL', "http://".$_SERVER['SERVER_NAME'] . "/designer_api" . DIRECTORY_SEPARATOR);
+
 }
 
 
